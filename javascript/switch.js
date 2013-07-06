d25e989d1fee47a47c5e4a8afea6a39c893ea0ea
mentalmarriott <firemazza@gmail.com>
2013-07-06 17:15:04 +0100

    trying to get canvas to resize to window

---
+++ javascript/switch.js
@@ -0,0 +1,12 @@
+//this javascript is used to switch the information panes on the canvas to links to appropriate information
+
+function resize()
+{
+	var canvas = document.getElementById('myCanvas');
+	var ctx = canvas.getContext('2d');
+
+	document.getElementById('canvasDiv').style.height = 1000;
+	
+//	ctx.canvas.width = window.innerWidth;
+//	ctx.canvas.height = window.innerHeight;
+}
