e3def471764ad1e982b300fdcd24d88b255e563e
mentalmarriott <firemazza@gmail.com>
2013-07-07 22:50:11 +0100

    working on auto resizing canvas

--- javascript/switch.js
+++ javascript/switch.js
@@ -1,12 +1,26 @@
 //this javascript is used to switch the information panes on the canvas to links to appropriate information
 
-function resize()
-{
-	var canvas = document.getElementById('myCanvas');
-	var ctx = canvas.getContext('2d');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 
-	document.getElementById('canvasDiv').style.height = 1000;
+ctx.fillStyle = 'white';
+ctx.font = 'bold 40px Monospace';
+ctx.textBaseline = 'top';
+ctx.fillText('hello', 10, 10);
+
+function resize() {
+	// Our canvas must cover full height of screen
+	// regardless of the resolution
+	var height = window.innerHeight;
 	
-//	ctx.canvas.width = window.innerWidth;
-//	ctx.canvas.height = window.innerHeight;
+	// So we need to calculate the proper scaled width
+	// that should work well with every resolution
+	var ratio = canvas.width/canvas.height;
+	var width = height * ratio;
+	
+	canvas.style.width = width+'px';
+	canvas.style.height = height+'px';
 }
+
+window.addEventListener('load', resize, false);
+window.addEventListener('resize', resize, false);
