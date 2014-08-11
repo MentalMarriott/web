9d647fa91ba117d05a84c7c82a1c7947b417630e
mentalmarriott <firemazza@gmail.com>
2014-08-11 22:02:57 +0100

    modifications to colour and location of menu

--- javascript/switch.js
+++ javascript/switch.js
@@ -3,13 +3,13 @@
 function init()
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	canvas.width = 1200;
+	canvas.width = 1000;
 	canvas.height = 800;
 	document.body.style.background="black";
-	document.getElementById('myCanvas').style.background = '#0B6121';	
+	document.getElementById('myCanvas').style.background = '#E5E4E2';	
 
 	canvas.addEventListener('mousemove', choiceHover, false);
-        canvas.addEventListener('mousedown', choiceClick, false);
+	canvas.addEventListener('mousedown', choiceClick, false);
 
 	mainMenu();
 }
@@ -23,24 +23,25 @@ function choiceHover(ev)
 	x = pos[0];
 	y = pos[1];
 
-	var menu1Bounds = (x > 175) && (x < 300) && (y > 30) && (y < 65);	
+	var menu1Bounds = (x > (canvas.width/100)*13) && (x < ((canvas.width/100)*32)) && (y > 30) && (y < 65);	
 
 	if(menu1Bounds)
 	{
+		ctx.clearRect((canvas.width/100)*13, 60, 100, 10);
 		ctx.lineWidth=1;
-        	ctx.fillStyle="#ff0000";
-	        ctx.font="40px sans-serif";
-        	ctx.fillText("Menu 1", 140, 60);
-	}else{
-		ctx.lineWidth=1;
-        	ctx.fillStyle="#0B0B61";
-	        ctx.font="40px sans-serif";
-        	ctx.fillText("Menu 1", 140, 60);
+		ctx.fillStyle="#ff0000";
+		ctx.font="40px sans-serif";
+		ctx.fillText("Menu 1", (canvas.width/100)*13, 60);
+		console.log("WORKING");
+		   		                						}else{
+		//	ctx.lineWidth=1;
+		//	ctx.fillStyle="#0B0B61";
+		//      ctx.font="40px sans-serif";
+		//	ctx.fillText("Menu 1", (canvas.width/100)*13, 60);
 	}
 }
 
 
-/*selects menu or item hovering over at the time */
 function choiceClick(ev)
 {
 
@@ -50,46 +51,38 @@ function choiceClick(ev)
 function mainMenu()
 {
 	ctx.lineWidth=1;
-	ctx.fillStyle="#0B0B61";
+	ctx.fillStyle="#000000";
 	ctx.font="40px sans-serif";
-	ctx.fillText("Menu 1", 140, 60);
+	ctx.fillText("Menu 1", (canvas.width/100)*13, 60);
 	console.log("cake");
 
 	ctx.lineWidth=1;
-        ctx.fillStyle="#0B0B61";
+        ctx.fillStyle="#000000";
         ctx.font="40px sans-serif";
-        ctx.fillText("Menu 2", 340, 60);
+        ctx.fillText("Menu 2", (canvas.width/100)*33, 60);
 
 	ctx.lineWidth=1;
-        ctx.fillStyle="#0B0B61";
+        ctx.fillStyle="#000000";
         ctx.font="40px sans-serif";
-        ctx.fillText("Menu 3", 540, 60);
-
-
-	ctx.lineWidth=1;
-        ctx.fillStyle="#0B0B61";
-        ctx.font="40px sans-serif";
-        ctx.fillText("Menu 4", 740, 60);
-	
-	ctx.lineWidth=1;
-        ctx.fillStyle="#0B0B61";
-        ctx.font="40px sans-serif";
-        ctx.fillText("Menu 5", 940, 60);
+        ctx.fillText("Menu 3", (canvas.width/100)*53, 60);
 
 	ctx.lineWidth=1;
-        ctx.fillStyle="#0B0B61";
+        ctx.fillStyle="#000000";
+        ctx.font="40px sans-serif";
+        ctx.fillText("Menu 4", (canvas.width/100)*73, 60);
+
+	ctx.lineWidth=1;
+	ctx.fillStyle="#000000";
         ctx.font="40px sans-serif";
         ctx.fillText("Ignore Colour not final design", canvas.width/2, canvas.height/2);
 
 	ctx.lineWidth = 10;	
-	ctx.moveTo(100, 70);
-	ctx.lineTo(1100, 70);
-	ctx.strokeStyle='#610B0B';
+	ctx.moveTo((canvas.width/100)*10, 70);
+	ctx.lineTo((canvas.width/100)*90, 70);
+	ctx.strokeStyle='#0000A0';
 	ctx.stroke();
+
 }
-
-
-
 
 
 
@@ -99,18 +92,15 @@ function mainMenu()
 var mouseLoc = function(ev)
 {
 	var x, y;
-
 	if(ev.layerX || ev.layerY === 0)
 	{//firefox
 		x = ev.layerX;
 		y = ev.layerY;
+	}else if (ev.offsetX || ev.offset === 0){//opera
+		x = ev.offsetX;				
+		y = ev.offsetY;		
+
 	}
-	else if (ev.offsetX || ev.offset === 0)
-	{//opera
-		x = ev.offsetX;
-		y = ev.offsetY;
-	}
-	
 	return [x, y];
 }
 
@@ -121,18 +111,17 @@ function mousePos(ev)
 {
 	var x, y;
 	var pos = mouseLoc(ev);
+
 	x = pos[0];
 	y = pos[1];
 	
 	var mouseP = document.getElementById("mousePos");
+	
 	mouseP.innerHTML = ("x= " + x + ", y= " + y);
+	
 	mouseP.style.backgroundColor = 'white';
+	
 }
-
-
-
-
-
 
 
 
